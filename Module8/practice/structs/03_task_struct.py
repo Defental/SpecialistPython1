61b29a52b0ee504fe05967dbe6934564b8948a3a
Defental <89096921+Defental@users.noreply.github.com>
2021-08-26 20:23:43 +0300

    Update 03_task_struct.py

--- Module8/practice/structs/03_task_struct.py
+++ Module8/practice/structs/03_task_struct.py
@@ -1,6 +1,13 @@
-# Дан список, заполненный произвольными целыми числами.
-# Примечание: для генерации списка используйте функцию.
-# Найдите:
-# 1. Количество элементов списка не превышающие 10
-# 2. Сумму всех положительных элементов списка
-# 3. Среднее арифметическое всех четных элементов
+import random
+
+def a_list(size, of=-10, to=10):
+    return[random.randint(of, to) for _ in range(size)]
+
+a = a_list(8)
+
+count = len(list(filter(lambda el: el <=10, a)))
+sum1 = sum(el for el in a if el > 0)
+sum2 = (sum(el for el in a if el%2 == 0)/len(list(el for el in a if el%2==0)))
+print(a)
+print(count)
+print(sum1)
